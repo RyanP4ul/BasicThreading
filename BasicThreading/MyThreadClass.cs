b16fbb89e333e986f6b03a9cad655c5ff69cd9a1
Espinola <espinola.253086@sticollegebalagtas.tk>
2025-09-18 18:17:51 +0800

    First Commit!

---
+++ BasicThreading/MyThreadClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThreading
+{
+    internal class MyThreadClass
+    {
+
+        public static void Thread1()
+        {
+            for (var i = 0; i < 6; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+                Thread.Sleep(1500);
+            }
+      
+        }
+
+    }
+}
