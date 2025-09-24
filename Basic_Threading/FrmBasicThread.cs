f4ecf0697318042bd2dee6e4f57c332898c88f8b
zanderesteban61@gmail.com <espinolaryanpaul2028@gmail.com>
2025-09-24 18:34:15 +0800

    Commit!

---
+++ Basic_Threading/FrmBasicThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Threading
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before starting thread-");
+
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread1);
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            lblMessage.Text = "-End of the thread";
+            Console.WriteLine("-End of the thread");
+        }
+    }
+}
